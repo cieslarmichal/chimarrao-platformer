59e58b1729f4c7cc9ce427cffd4804dd21b04ab4
Wojciech Sekta <wojtek1994pl@gmail.com>
2021-03-21 13:25:30 +0100

    add tile map serializer cpp

--- src/game/src/editor/TileMapSerializerJson.cpp
+++ src/game/src/editor/TileMapSerializerJson.cpp
@@ -12,13 +12,13 @@ TileMapSerializerJson::TileMapSerializerJson() {
 std::string TileMapSerializerJson::serialize(const std::vector<std::vector<Tile>>& vector)
 {
     nlohmann::json json = nlohmann::json().object();
-    for (int y = 0; y < vector.size(); y++)
-    {
-        for (int x = 0; x < vector[y].size(); x++)
-        {
-            json[x][y] = tileTypeToInt[vector[y][x].type];
-        }
-    }
+//    for (int y = 0; y < vector.size(); y++)
+//    {
+//        for (int x = 0; x < vector[y].size(); x++)
+//        {
+//            json[x][y] = tileTypeToInt[vector[y][x].type];
+//        }
+//    }
     return json.dump();
 }
 
