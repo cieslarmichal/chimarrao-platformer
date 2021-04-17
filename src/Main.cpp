8d271222ffe05d0afde02a97692705548f5da138
Michal Cieslar <michalovskyyy@gmail.com>
2021-04-17 11:30:52 +0200

    remove windows cmd popping when starting game executable

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,19 @@
 #include "Game.h"
 
+#include <Windows.h>
+
 int main()
 {
     game::Game game;
     game.run();
     return 0;
 }
+
+#ifdef _WIN32
+int APIENTRY WinMain(HINSTANCE hInstance,
+HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+return main();
+}
+#endif
