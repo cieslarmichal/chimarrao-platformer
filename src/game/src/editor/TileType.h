d27ba1371d9a1fee8571791f4398e53bc57c7c07
michalovsky <michalovskyyy@gmail.com>
2021-12-26 14:25:25 +0100

    add tree, bush, campfire and chest tile types

--- src/game/src/editor/TileType.h
+++ src/game/src/editor/TileType.h
@@ -12,12 +12,15 @@ namespace game
 enum class TileType
 {
     Brick,
-    Grass
+    Grass,
+    Tree,
+    Bush,
+    Campfire,
+    Chest
 };
 
 const std::vector<TileType> tileTypes{
-    TileType::Brick,
-    TileType::Grass,
+    TileType::Brick, TileType::Grass, TileType::Tree, TileType::Bush, TileType::Campfire, TileType::Chest,
 };
 
 const TileType defaultTileType = TileType::Brick;
@@ -33,7 +36,12 @@ inline std::string tileTypeToPathTexture(TileType type)
     const std::unordered_map<TileType, std::string> tileTextures{
         {TileType::Brick, utils::ProjectPathReader::getProjectRootPath() + "resources/Tiles/brick.png"},
         {TileType::Grass, utils::ProjectPathReader::getProjectRootPath() + "resources/Tiles/2.png"},
+        {TileType::Tree, utils::ProjectPathReader::getProjectRootPath() + "resources/tree.png"},
+        {TileType::Bush, utils::ProjectPathReader::getProjectRootPath() + "resources/bush.png"},
+        {TileType::Campfire, utils::ProjectPathReader::getProjectRootPath() + "resources/campfire.png"},
+        {TileType::Chest, utils::ProjectPathReader::getProjectRootPath() + "resources/chest.png"},
     };
+
     try
     {
         return tileTextures.at(type);
